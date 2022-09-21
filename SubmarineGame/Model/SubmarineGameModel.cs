f0337792fa127192d920720a4064000a1a94f305
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-21 16:18:58 +0200

    Add properties and constructors to Model

--- SubmarineGame/Model/SubmarineGameModel.cs
+++ SubmarineGame/Model/SubmarineGameModel.cs
@@ -24,5 +24,40 @@ namespace Model
         private IPersistence _persistence;
 
         #endregion
+
+        #region Public fields
+
+        public int gameTime;
+
+        #endregion
+
+        #region Events
+
+        #endregion
+
+        #region Properties
+
+        public Shape Submarine { get { return _submarine; } }
+        public IList<Shape> Mines { get { return _mines.AsReadOnly(); } }
+
+        #endregion
+
+        #region Constructors
+
+        public SubmarineGameModel() : this(null) { }
+        public SubmarineGameModel(IPersistence persistence)
+        {
+            _destroyedMineCount = 0;
+            gameTime = 0;
+
+            _random = new Random();
+
+            _submarine = new Shape(ShapeType.Submarine, (GameAreaWidth - SubmarineSize) / 2, GameAreaHeight - SubmarineSize, SubmarineSize, SubmarineSize, 1);
+            _mines = new List<Shape>();
+
+            _persistence = persistence;
+        }
+
+        #endregion
     }
 }
