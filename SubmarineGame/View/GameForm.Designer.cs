c531c4f618fb438f379352b22391c991e82337b2
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-21 16:39:43 +0200

    Add design to View

--- SubmarineGame/View/GameForm.Designer.cs
+++ SubmarineGame/View/GameForm.Designer.cs
@@ -28,12 +28,173 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripGameTime = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripTime = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripDestroyedMines = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripDestroyedMineCount = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.submarine = new System.Windows.Forms.PictureBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.submarine)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripGameTime,
+            this.toolStripTime,
+            this.toolStripDestroyedMines,
+            this.toolStripDestroyedMineCount});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 639);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(924, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripGameTime
+            // 
+            this.toolStripGameTime.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripGameTime.Name = "toolStripGameTime";
+            this.toolStripGameTime.Size = new System.Drawing.Size(70, 17);
+            this.toolStripGameTime.Text = "Game Time:";
+            // 
+            // toolStripTime
+            // 
+            this.toolStripTime.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripTime.Name = "toolStripTime";
+            this.toolStripTime.Size = new System.Drawing.Size(13, 17);
+            this.toolStripTime.Text = "0";
+            // 
+            // toolStripDestroyedMines
+            // 
+            this.toolStripDestroyedMines.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripDestroyedMines.Name = "toolStripDestroyedMines";
+            this.toolStripDestroyedMines.Size = new System.Drawing.Size(98, 17);
+            this.toolStripDestroyedMines.Text = "Destroyed Mines:";
+            // 
+            // toolStripDestroyedMineCount
+            // 
+            this.toolStripDestroyedMineCount.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripDestroyedMineCount.Name = "toolStripDestroyedMineCount";
+            this.toolStripDestroyedMineCount.Size = new System.Drawing.Size(13, 17);
+            this.toolStripDestroyedMineCount.Text = "0";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(924, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.loadGameToolStripMenuItem,
+            this.saveGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.newGameToolStripMenuItem.Text = "New game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.MenuFile_NewGame);
+            // 
+            // loadGameToolStripMenuItem
+            // 
+            this.loadGameToolStripMenuItem.Name = "loadGameToolStripMenuItem";
+            this.loadGameToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.loadGameToolStripMenuItem.Text = "Load game...";
+            this.loadGameToolStripMenuItem.Click += new System.EventHandler(this.MenuFile_LoadGame);
+            // 
+            // saveGameToolStripMenuItem
+            // 
+            this.saveGameToolStripMenuItem.Name = "saveGameToolStripMenuItem";
+            this.saveGameToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.saveGameToolStripMenuItem.Text = "Save game...";
+            this.saveGameToolStripMenuItem.Click += new System.EventHandler(this.MenuFile_SaveGame);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.MenuFile_Exit);
+            // 
+            // submarine
+            // 
+            this.submarine.Image = global::View.Properties.Resources.submarine;
+            this.submarine.Location = new System.Drawing.Point(402, 572);
+            this.submarine.Name = "submarine";
+            this.submarine.Size = new System.Drawing.Size(64, 64);
+            this.submarine.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.submarine.TabIndex = 2;
+            this.submarine.TabStop = false;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.Filter = "Submarine Game (*.smg)|*.smg";
+            this.openFileDialog.Title = "Loading Submarine Game";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Filter = "Submarine Game (*.smg)|*.smg";
+            this.saveFileDialog.Title = "Save Submarine Game";
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(924, 661);
+            this.Controls.Add(this.submarine);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "GameForm";
+            this.Text = "Submarine Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GameForm_KeyDown);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.submarine)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripGameTime;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripTime;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripDestroyedMines;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripDestroyedMineCount;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.PictureBox submarine;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
     }
 }
