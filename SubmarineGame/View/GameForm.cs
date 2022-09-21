bfae8015f5e76e7a6d80eaffe3d8ac6f1ff38328
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-21 16:43:53 +0200

    Add private fields to GameForm in View

--- SubmarineGame/View/GameForm.cs
+++ SubmarineGame/View/GameForm.cs
@@ -1,7 +1,22 @@
+using System.Diagnostics;
+using System.Windows.Forms;
+using Model;
+
 namespace View
 {
     public partial class GameForm : Form
     {
+        #region Private fields
+
+        private SubmarineGameModel _model;
+        private List<PictureBox> _mines;
+        private Stopwatch _stopWatch;
+        private Timer _timer;
+        private Timer _gameTimer;
+        private Timer _minesTimer;
+
+        #endregion
+
         public GameForm()
         {
             InitializeComponent();
