fec57d477d53c9188d6b29ed803611105bdb3d9f
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-21 16:05:53 +0200

    Add Model project

--- SubmarineGame/View/GameForm.cs
+++ SubmarineGame/View/GameForm.cs
@@ -1,8 +1,8 @@
 namespace View
 {
-    public partial class Form1 : Form
+    public partial class GameForm : Form
     {
-        public Form1()
+        public GameForm()
         {
             InitializeComponent();
         }
