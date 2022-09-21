c379b8ed644b3b393862c8c8c62d82e444c2287c
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-21 16:35:10 +0200

    Define IPersistence interface in Persistence

--- SubmarineGame/Persistence/IPersistence.cs
+++ SubmarineGame/Persistence/IPersistence.cs
@@ -1,7 +1,8 @@
 ﻿namespace Persistence
 {
-    public class IPersistence
+    public interface IPersistence
     {
-
+        List<Shape> Load(String path, ref Int32 gameTime, ref Int32 destroyedMineCount);
+        void Save(String path, List<Shape> mines, Shape submarine, Int32 gameTime, Int32 destroyedMineCount);
     }
 }
