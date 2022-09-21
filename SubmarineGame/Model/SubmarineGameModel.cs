c22917e524862829036e2e4c65825201a9e27362
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-21 16:14:29 +0200

    Add constants and private fields to Model and implement relevant classes in Persistence

--- SubmarineGame/Model/SubmarineGameModel.cs
+++ SubmarineGame/Model/SubmarineGameModel.cs
@@ -1,7 +1,28 @@
-﻿namespace Model
+﻿using Persistence;
+
+namespace Model
 {
     public class SubmarineGameModel
     {
+        #region Constants
 
+        public const int GameAreaWidth = 940;
+        public const int GameAreaHeight = 700;
+        public const int SubmarineSize = 64;
+        public const int MineSize = 64;
+        public const int SubmarineStep = 10;
+        public const int MineStep = 5;
+
+        #endregion
+
+        #region Private fields
+
+        private Shape _submarine;
+        private List<Shape> _mines;
+        private Random _random;
+        private int _destroyedMineCount;
+        private IPersistence _persistence;
+
+        #endregion
     }
 }
--- SubmarineGame/Persistence/IPersistence.cs
+++ SubmarineGame/Persistence/IPersistence.cs
@@ -1,6 +1,6 @@
 ﻿namespace Persistence
 {
-    public class Class1
+    public class IPersistence
     {
 
     }
